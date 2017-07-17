5df9210e3310b75b293ce662ac0cc62d2eb05fe1
jiyoungdream <15dream@naver.com>
2017-07-17 11:25:01 +0900

    first commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero'; import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor(private heroservice : HeroService) { }
+
+  heroes : Hero[];
+
+  ngOnInit() {
+    this.heroservice.getHeroes()
+    .then(heroes => this.heroes = heroes.slice(1,5));
+  }
+}
